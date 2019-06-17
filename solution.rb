04281c1befa2e72d6ee594e1f795bc12fc941e00
Julián <judramirezva@gmail.com>
2019-06-17 18:32:29 -0500

    Reto Abuela Sorda - Sinatra

---
+++ solution.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+
+get '/' do
+	@abuela = params[:abuela]
+	erb :abuelita
+end
+
+post '/abuela' do
+  if params[:input_usuario] == params[:input_usuario].upcase
+    @abuela = "manzana"
+  else
+    @abuela = "Habla mas duro mijito"
+  end
+  redirect "/?abuela=#{@abuela}"
+end
